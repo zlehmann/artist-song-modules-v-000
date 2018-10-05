58ed065024cd9c458b57375f0cf2bf98297a1ca0
Zachary Lehmann <zlehmann@gmail.com>
2018-10-05 21:35:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+  #  @@songs.detect{|a| a.name == name}
+  #end
 
   def self.all
     @@songs

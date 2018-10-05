9928374c133b18bedb12e840c8b2865b80dd9da7
Zachary Lehmann <zlehmann@gmail.com>
2018-10-05 21:39:53 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -30,7 +30,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 end

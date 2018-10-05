816b0a36acdf8686ac2f79e1c348f971759c37c9
Zachary Lehmann <zlehmann@gmail.com>
2018-10-05 21:19:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,13 +18,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #def self.reset_all
+  #  self.all.clear
+  #end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+  #  self.all.count
+  #end
 
   def artist=(artist)
     @artist = artist

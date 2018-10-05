1f0422724b683b3a725dd9977f61b01109d64a81
Zachary Lehmann <zlehmann@gmail.com>
2018-10-05 21:35:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+  #  @@artists.detect{|a| a.name == name}
+  #end
 
   def self.all
     @@artists
--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,5 @@
 module findable
   def find_by_name(name)
     self.all.detect{|a| a.name == name}
-  end 
+  end
 end 
